8195a2c8fe0f6dc42b2c648f110d6f36853b009d
MisanthropeGirl <mail@clarissadarling.me.uk>
2025-10-29 13:19:52 +0000

    Testing App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,12 @@
 import { render } from './test-utils';
 import App from './App';
 
+// Mock Highcharts
+jest.mock("highcharts", () => ({}));
+jest.mock("highcharts-react-official", () => ({
+  HighchartsReact: () => null,
+}));
+
 test('renders without crashing', () => {
   render(<App />);
 });
